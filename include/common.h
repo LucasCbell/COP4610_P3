959d62629a1e7bdeae69a7540371210d567d4eca
Zachary Skalski-Fouts <zqs23a@fsu.edu>
2025-11-17 12:22:54 -0500

    Cleaned up directories

--- include/common.h
+++ include/common.h
@@ -1,10 +1,4 @@
 #pragma once
 
-#include "commands.h"
-#include "directory_ops.h"
-#include "fat32.h"
-#include "file_ops.h"
 #include "lexer.h"
-#include "navigation.h"
 #include "shell.h"
-#include "utils.h"
