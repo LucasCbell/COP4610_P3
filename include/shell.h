01b1c30598ec745ecd76f6e72ed929bac65eba26
LucasCbell <lucasdcampbell03@gmail.com>
2025-12-05 16:35:47 -0500

    Update shell.h

--- include/shell.h
+++ include/shell.h
@@ -1,1 +1,8 @@
+#pragma once
 
+extern char current_path[256];
+
+void print_image_name(char* img_name);
+void print_path(char* current_path);
+void init_path(void);
+void update_path(char* dirname, int is_entering);
