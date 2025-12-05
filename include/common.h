68a3f2e6575083c9764eef7bbc1d15c98843c8e8
LucasCbell <lucasdcampbell03@gmail.com>
2025-12-05 16:34:54 -0500

    Create common.h

---
+++ include/common.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "lexer.h"
+#include "shell.h"
+#include "file_ops.h"
+#include "commands.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
